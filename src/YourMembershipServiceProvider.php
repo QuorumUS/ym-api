c0c5ed44ec8d0402acdab3f7bed4f56408d68f24
Peter Arapov <petr@phone2action.com>
2017-01-12 16:07:50 -0500

    Fix ServiceProvider

--- src/YourMembershipServiceProvider.php
+++ src/YourMembershipServiceProvider.php
@@ -6,48 +6,58 @@ use P2A\YourMembership\YourMembershipClient;
 
 class YourMembershipServiceProvider extends ServiceProvider
 {
-
-	private $packageName = 'yourmembership';
-	private $packageConfigPath = __DIR__.'/config/yourmembership.php';
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
+	 * Package Config Name
+	 * @var string
 	 */
-	protected $defer = true;
-
+    private $packageName = 'yourmembership';
 	/**
-	 * Register the service provider.
+	 * Package Config Path
+	 * @var string
+	 */
+    private $packageConfigPath = __DIR__.'/config/yourmembership.php';
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            $this->packageConfigPath, $this->packageName
+        );
+
+        $this->app->bind(YourMembershipClient::class, function ($app, $parameters) {
+            $guzzleClient = new \GuzzleHttp\Client($app['config']['yourmembership']['guzzle-client']);
+            return new YourMembershipClient($guzzleClient, $parameters[0], $parameters[1]);
+        });
+    }
+	/**
+	 * Boot the service provider.
 	 *
 	 * @return void
 	 */
-	public function register()
-	{
-		$this->mergeConfigFrom(
-	    	$this->packageConfigPath, $this->packageName
-		);
+    public function boot()
+    {
+        $this->publishes([
+            $this->packageConfigPath => config_path('yourmembership.php'),
+        ]);
+    }
 
-		$this->app->register(YourMembershipClient::class, function($app, $parameters) {
-			$guzzleClient = new \GuzzleHttp\Client($app['config']['yourmembership']['guzzle-client']);
-			return new YourMembershipClient($guzzleClient, $parameters[0], $parameters[1]);
-		});
-	}
-
-	public function boot()
-	{
-		$this->publishes([
-		$this->packageConfigPath => config_path('yourmembership.php'),
-		]);
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array(YourMembershipClient::class);
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [YourMembershipClient::class];
+    }
 }
