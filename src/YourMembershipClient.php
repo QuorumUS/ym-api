b7632e19f17563aa8a0bbdf083b043bfe7a88b20
Aaron Boyarsky <aboyarsky@phone2action.com>
2017-01-26 14:55:08 -0500

    When YM API responds with XML indicating an error, throw a ResponseException with the error info

--- src/YourMembershipClient.php
+++ src/YourMembershipClient.php
@@ -5,6 +5,7 @@ namespace P2A\YourMembership;
 use GuzzleHttp\Client;
 use P2A\YourMembership\Core\Request;
 use P2A\YourMembership\Core\Response;
+use P2A\YourMembership\Exceptions\YourMembershipResponseException;
 
 class YourMembershipClient
 {
@@ -32,10 +33,12 @@ class YourMembershipClient
 	 * Makes API Call to YourMembership
 	 * @method makeCall
 	 * @author PA
+     * @author AB http://github.com/chefboyarsky
 	 * @date   2017-01-10
 	 * @param  string     $method    Your Membership API Method
 	 * @param  array      $arguments  Your Membership API Call Arguments
 	 * @return Response
+     * @throws YourMembershipResponseException If the response from the API has an error
 	 */
 	public function makeCall(string $method, array $arguments = [])
 	{
@@ -43,9 +46,14 @@ class YourMembershipClient
 		Request::$callId++; //Update the Call ID as they need to be unique per call
 		$request = $this->request->buildRequest($method, $arguments);
 
-		$response = $this->client->send($request);
+		$response = new Response($method, $this->client->send($request));
 
-		return new Response($method, $response);
+        if($response->hasError())
+        {
+            throw new YourMembershipResponseException($response->getError(), $response->getErrorCode(), $method);
+        }
+
+		return $response;
 	}
 
 	/**
